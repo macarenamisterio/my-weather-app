09db8eb91d514aacd2e9f580d68ffe9d15a70e5e
macarenamisterio <macarena.misterio@gmail.com>
2022-01-15 21:45:37 +1100

    Added geolocation and current temp

--- src/app.js
+++ src/app.js
@@ -1,4 +1,17 @@
-let apiKey = "99f763cf958e5832295c470b28782d08"
+function currentWeather(response) {
+  let temp = Math.round(response.data.main.temp);
+  let tempNow = document.querySelector("#temp");
+  tempNow.innerHTML = `${temp}°C`;
+}
 
-let now = new Date();
-document.getElementById("date-time").innerHTML = `${now.toLocaleString()}`;
+function logPosition(position) {
+  let apiKey = "99f763cf958e5832295c470b28782d08";
+  let latitude = position.coords.latitude;
+  let longitude = position.coords.longitude;
+  let units = "metric";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&units=${units}&appid=${apiKey}`;
+  axios.get(apiUrl).then(currentWeather);
+}
+
+navigator.geolocation.getCurrentPosition(logPosition);
+
